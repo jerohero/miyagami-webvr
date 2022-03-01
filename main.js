03367c1a5cf01eec3bac31aa2a02bd6f92a4d770
Jeroen <jegbol01@gmail.com>
2022-03-01 17:30:32 +0100

    Added start text

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
 let environmentEl;
+let startTextEl;
+
 const presets = ['forest', 'egypt', 'japan', 'dream', 'volcano', 'goldmine', 'tron', 'starry'];
 let activePreset = 'forest';
 
 window.onload = () => {
     environmentEl = document.getElementById('environment');
+    startTextEl = document.getElementById('start-text');
 }
 
 const nextPreset = () => {
+    if (startTextEl) {
+        hideStartText();
+    }
+
     const nextPreset = presets[presets.indexOf(activePreset) + 1] || presets[0];
 
     environmentEl.setAttribute('environment', `preset:${ nextPreset }`);
 
     activePreset = nextPreset;
+}
+
+const hideStartText = () => {
+    startTextEl.remove();
+    startTextEl = null;
 }
