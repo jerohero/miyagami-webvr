9c9eb68780d91ab7fadd8f71fe9247e45d8ea19d
Jeroen <jegbol01@gmail.com>
2022-03-01 16:20:23 +0100

    Functionality for updating preset

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-    document.getElementById('environment').setAttribute('environment', 'preset:checkerboard')
+let environmentEl;
+
+const presets = [
+    'forest',
+    'egypt',
+    'japan',
+    'dream',
+    'volcano',
+    'starry'
+];
+let activePreset = 'forest';
+
+window.onload = () => {
+    environmentEl = document.getElementById('environment');
+}
+
+const nextPreset = () => {
+    const nextPreset = presets[presets.indexOf(activePreset) + 1];
+
+    environmentEl.setAttribute('environment', `preset:${ nextPreset }`);
+
+    activePreset = nextPreset;
+}
